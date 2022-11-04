7536722964c69fd8806d67cfbe967df165182ab0
Pureddy Lavanya <pureddylavanya2002@gmail.com>
2022-11-04 22:10:56 +0530

    Initial commit

---
+++ min.js
@@ -0,0 +1,10 @@
+function get(){
+    var nm=document.getElementById("name").value;
+    var d1=document.getElementById("age").value;
+   var d2=new Date();
+   var d3=new Date(d1);
+   var d4=new Date(d2);
+   var d5=Math.floor(((d4.getTime()-d3.getTime())/(1000*3600*24))/365);
+   var str="Hi  "+nm+"\n"+"\n"+"Your Age is"+d5;
+   alert(str);
+}
