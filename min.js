57fd0cca4956bf3f2eefb44f91c6c4fa23232a8e
PureddyLavanya89 <pureddylavanya2002@gmail.com>
2023-07-05 06:23:29 +0530

    Update min.js

--- min.js
+++ min.js
@@ -5,6 +5,13 @@ function get(){
    var d3=new Date(d1);
    var d4=new Date(d2);
    var d5=Math.floor(((d4.getTime()-d3.getTime())/(1000*3600*24))/365);
+   if d5>=1
+    {
    var str="Hi  "+nm+"\n"+"\n"+"Your Age is"+d5;
    alert(str);
+    }
+  else
+    {
+    alert('Enter a valid input');
+    }    
 }
